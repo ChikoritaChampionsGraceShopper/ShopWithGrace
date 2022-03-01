018da5addcfe184ff31f1f07795b3bc173e02dcc
Edwin Detrez <edetrezjr@gmail.com>
2022-03-01 17:01:55 -0500

    Edit user model

---
+++ server/db/models/Product.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+
+
+})
