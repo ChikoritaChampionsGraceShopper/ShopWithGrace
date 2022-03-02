969b6fb66f15d3d92b73b9b8bbe16eb1a383337b
Chao Zhou <Czhou0k@gmail.com>
2022-03-01 22:10:03 -0500

    added the login and signup route to HomePage component

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login, Signup } from './components/AuthForm';
 
 const HomePage = () => {
   return (
     <div>
-      <nav></nav>
+      <nav>
+        <Switch>
+          <Route path='/login'>{Login}</Route>
+          <Route path='/signup'>{Signup}</Route>
+        </Switch>
+      </nav>
       <div>
         <h1>I don't know why it is boujee, but it is cool!</h1>
       </div>
