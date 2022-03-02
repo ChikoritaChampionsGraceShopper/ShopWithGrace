d4b917e35e97673c1da5b40681d6a064ec56cbc2
Chao Zhou <Czhou0k@gmail.com>
2022-03-01 20:28:16 -0500

    created an HomePage component

---
+++ client/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <nav></nav>
+      <div>
+        <h1>I don't know why it is boujee, but it is cool!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
