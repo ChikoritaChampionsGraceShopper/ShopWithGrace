a5c5a608a2896fcb72a95994b3a37608c7bb8c5a
Chao Zhou <Czhou0k@gmail.com>
2022-03-03 22:46:44 -0500

    fixing files

--- client/Routes.js
+++ client/Routes.js
@@ -1,13 +1,12 @@
-import React, { Component, Fragment, useEffect } from "react";
-import { connect, useSelector, useDispatch } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
-import { me } from "./store";
-import AllProducts from "./components/AllProducts";
-import Navbar from "./components/Navbar";
-import SingleProduct from "./components/SingleProduct";
-import HomePage from "./components/HomePage";
+import React, { Component, Fragment, useEffect } from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login, Signup } from './components/AuthForm';
+import Home from './components/Home';
+import { me } from './store';
+import AllProducts from './components/AllProducts';
+import SingleProduct from './components/SingleProduct';
+import HomePage from './components/HomePage';
 
 /**
  * COMPONENT
@@ -23,29 +22,18 @@ const Routes = () => {
 
   return (
     <div>
-      {/* <nav>
-        <Navbar />
-      </nav> */}
       {isLoggedIn ? (
         <Switch>
-          <Route path="/home" component={Home} />
-          <Redirect to="/home" />
+          <Route path='/home' component={Home} />
+          <Redirect to='/home' />
         </Switch>
       ) : (
         <Switch>
-<<<<<<< HEAD
           <Route exact path='/' component={HomePage} />
           <Route path='/login'>{Login}</Route>
           <Route path='/signup'>{Signup}</Route>
           <Route exact path='/products' component={AllProducts} />
           <Route exact path='/products/:id' component={SingleProduct} />
-=======
-          <Route exact path="/" component={HomePage} />
-          <Route path="/login">{Login}</Route>
-          <Route path="/signup">{Signup}</Route>
-          <Route exact path="/products" component={AllProducts} />
-          <Route exact path="/products/:id" component={SingleProduct} />
->>>>>>> 49651c72112309f6a32f3fca91f932635a52d5a5
         </Switch>
       )}
     </div>
--- client/components/Cart/AddToCart.js
+++ client/components/Cart/AddToCart.js
@@ -1,34 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const addToCart = (item) => {
-    let addIt = true;
-    for (let i = 0; i < cart.length; i++){
-        if (cart[i].id === item.id) {
-            addIt = false
-        } if (addIt) {
-            setCart([...cart], item)
-        } else {
-            // increase quantity
-        } 
+  let addIt = true;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === item.id) {
+      addIt = false;
     }
-}
+    if (addIt) {
+      setCart([...cart], item);
+    } else {
+      // increase quantity
+    }
+  }
+};
 
 const AddToCart = () => {
-    const [cart, setCart] = useState([]);
-    return (
-        <div>
-            {addToCart}
-        </div>
-    )
-}
-
-
-
+  const [cart, setCart] = useState([]);
+  return <div>{addToCart}</div>;
+};
 
 // const AddToCart = () => {
 
 //   const addToCart = (item) => setCart((currentCart) => [...currentCart, item]);
-
 
 //   const amountOfItems = (id) => cart.filter((item) => item.id === id).length;
 
