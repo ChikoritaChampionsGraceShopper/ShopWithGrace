759d6b32a317470396e5cfa7eb4d67f84ac8498e
Chao Zhou <Czhou0k@gmail.com>
2022-03-05 11:16:26 -0500

    added an image and some css for notfound page

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -3,7 +3,14 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>Sorry The Page You Are Looking For Does Not Exist</h1>
+      <h1 className='notfound'>
+        Sorry The Page You Are Looking For Does Not Exist!
+      </h1>
+      <img
+        className='notfound1'
+        src='https://previews.123rf.com/images/atoss/atoss1310/atoss131000044/23130501-%EB%B9%88-%EC%B0%A8-%EC%BB%B5%EC%9E%85%EB%8B%88%EB%8B%A4-%ED%81%B4%EB%A6%AC%ED%95%91-%EA%B2%BD%EB%A1%9C.jpg'
+        width='300'
+      />
     </div>
   );
 };
