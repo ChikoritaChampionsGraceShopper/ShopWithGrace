263a7398e8722d1156b1f1af5a96cad9213603a6
Chao Zhou <Czhou0k@gmail.com>
2022-03-05 11:06:42 -0500

    made a NotFound Page and added to routes

--- client/Routes.js
+++ client/Routes.js
@@ -1,16 +1,16 @@
-
-import React, { Component, Fragment, useEffect } from "react";
-import { connect, useSelector, useDispatch } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
-import { me } from "./store";
-import AllProducts from "./components/AllProducts";
-import Navbar from "./components/Navbar";
-import SingleProduct from "./components/SingleProduct";
-import HomePage from "./components/HomePage";
-import AccountPage from './components/AccountPage'
-import CartPage from './components/Cart/CartPage'
+import React, { Component, Fragment, useEffect } from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login, Signup } from './components/AuthForm';
+import Home from './components/Home';
+import { me } from './store';
+import AllProducts from './components/AllProducts';
+import Navbar from './components/Navbar';
+import SingleProduct from './components/SingleProduct';
+import HomePage from './components/HomePage';
+import AccountPage from './components/AccountPage';
+import CartPage from './components/Cart/CartPage';
+import NotFound from './components/NotFound';
 
 const Routes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.id);
@@ -25,7 +25,7 @@ const Routes = () => {
       {isLoggedIn ? (
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path="/home" component={Home} />
+          <Route path='/home' component={Home} />
           <Route path='/account' component={AccountPage} />
           <Route exact path='/products' component={AllProducts} />
           <Route exact path='/products/:id' component={SingleProduct} />
@@ -39,6 +39,7 @@ const Routes = () => {
           <Route exact path='/products' component={AllProducts} />
           <Route exact path='/products/:id' component={SingleProduct} />
           <Route exact path='/cart' component={CartPage} />
+          <Route path='/*' component={NotFound} />
         </Switch>
       )}
     </div>
---
+++ client/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Sorry The Page You Are Looking For Does Not Exist</h1>
+    </div>
+  );
+};
+
+export default NotFound;
