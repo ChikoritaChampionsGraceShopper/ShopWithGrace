6a2a1c2f7f5d027c3e2736fa7cf6bee189f18caf
Harrison Lynch <hblynch2001@gmail.com>
2022-03-05 10:24:51 -0800

    added seed data for cart and orders

---
+++ script/CartSeedData.js
@@ -0,0 +1,39 @@
+const cartData = [{
+  id: 1,
+  quantity: 4,
+  price: 40,
+  orderId: 1,
+  productId: 7
+// },{
+//   id: 2,
+//   quantity: 1,
+//   price: 40,
+//   orderId: 2,
+//   productId: 3
+// },{
+//   id: 3,
+//   quantity: 4,
+//   price: 40,
+//   orderId: 1,
+//   productId: 7
+// },{
+//   id: 4,
+//   quantity: 2,
+//   price: 20,
+//   orderId: 3,
+//   productId: 7
+// },{
+//   id: 5,
+//   quantity: 1,
+//   price: 10,
+//   orderId: 1,
+//   productId: 5
+// },{
+//   id: 6,
+//   quantity: 4,
+//   price: 40,
+//   orderId: 1,
+//   productId: 1
+}]
+
+module.exports = cartData
---
+++ script/OrderSeedData.js
@@ -0,0 +1,23 @@
+const userOrders = [{
+  status: 'Fulfilled',
+},{
+  status: 'Unfulfilled',
+},{
+  status: 'Fulfilled',
+},{
+  status: 'Unfulfilled',
+},{
+  status: 'Fulfilled',
+},{
+  status: 'Unfulfilled',
+},{
+  status: 'Fulfilled',
+},{
+  status: 'Unfulfilled',
+},{
+  status: 'Fulfilled',
+},{
+  status: 'Unfulfilled',
+}]
+
+module.exports = userOrders
