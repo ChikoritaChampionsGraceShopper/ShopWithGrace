56fe7a5bbe7dfe4b63f2e2c874ee427f952d56b0
Edwin Detrez <edetrezjr@gmail.com>
2022-03-07 10:38:29 -0500

    Changed Recommended section  into sepereate component, improved homepage css

---
+++ client/components/Recommendation.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect, useContext } from "react";
+import FeaturedProducts from "./FeatureProducts";
+import { useProducts } from "./ProductsProvider";
+import Product from "./Product";
+import { Link } from "react-router-dom";
+import { CartContext, useCart } from "./Cart/CartProvider";
+
+const Recommendation = () => {
+  const { products, isLoading, mapArr, setSingleProduct } = useProducts();
+  const { addToCart } = useContext(CartContext);
+  console.log(mapArr);
+
+  return (
+    <div>
+      <div>
+        {mapArr[0] === undefined ? (
+          <div>Loading Data</div>
+        ) : (
+          <div>
+            <div className="recommended-title">
+              <h1>Recommended Products</h1>
+            </div>
+            <div className="recommended-container">
+              {mapArr.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <Link
+                      to={`/products/${item.id}`}
+                      onClick={() => setSingleProduct(item.id)}
+                    >
+                      <Product product={item} />
+                    </Link>
+                    <button onClick={() => addToCart(item)}>Add to Cart</button>
+                    <br />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Recommendation;
