5e586a69a39255129ccd80e56703e8be69ebf189
Edwin Detrez <edetrezjr@gmail.com>
2022-03-08 08:54:09 -0500

    Fixed redirect on login and Styling on prouct cards

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,31 +1,33 @@
-import React, { useContext, useEffect } from 'react'
-import { useProducts } from './ProductsProvider'
-import Product from './Product'
-import { CartContext } from './Cart/CartProvider'
+import React, { useContext, useEffect } from "react";
+import { useProducts } from "./ProductsProvider";
+import Product from "./Product";
+import { CartContext } from "./Cart/CartProvider";
 
-const SingleProduct = ({match}) => {
-  const { product, isLoading, setSingleProduct } = useProducts()
-  const { addToCart } = useContext(CartContext)
-  const { id } = match.params
+const SingleProduct = ({ match }) => {
+  const { product, isLoading, setSingleProduct } = useProducts();
+  const { addToCart } = useContext(CartContext);
+  const { id } = match.params;
 
   useEffect(() => {
-    setSingleProduct(id)
-  }, [])
+    setSingleProduct(id);
+  }, []);
 
   return (
-    <div className="singleProductContainer" key={product.id} >
-    { isLoading
-      ? <div className='loading'>Loading Product...</div>
-      : <div className='singleProductCard'>
-        <Product product={product} />
-      <div className="singleProductDescription" >
-        {product.description}
-      <button onClick={() => addToCart(product)} >Add to Cart</button>
+    <div className="singleProductContainer" key={product.id}>
+      {isLoading ? (
+        <div className="loading">Loading Product...</div>
+      ) : (
+        <div className="singleProductCard">
+          <div>
+            <Product product={product} />
+          </div>
+          <div className="single-product-addToCart">
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
         </div>
-      </div>
-      }
-  </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
